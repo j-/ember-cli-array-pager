d429bda155abf34511eed931d598af6e6cfbd7f9
j- <jh@live.com.au>
2015-02-07 12:28:29 +1100

    Refactored array pager logic

--- addon/array-pager.js
+++ addon/array-pager.js
@@ -1,21 +1,48 @@
+import Em from 'ember';
 import ArraySlice from 'array-slice';
+var get = Em.get;
+var set = Em.set;
 
 var ArrayPager = ArraySlice.extend({
-	incrementPage: function (pages) {
-		var limit = this.get('limit');
-		if (pages == null) {
-			pages = 1;
+	// 1-based
+	page: function (key, page) {
+		var limit = get(this, 'limit');
+		var offset;
+		// default value
+		if (arguments.length <= 1) {
+			offset = get(this, 'offset') || 0;
+			return Math.ceil(offset / limit) || 1;
 		}
-		limit *= pages;
-		return this.incrementProperty('offset', limit);
-	},
-	decrementPage: function (pages) {
-		if (pages == null) {
-			pages = 1;
+		// setter
+		// no negative pages
+		page = Math.max(page - 1, 0);
+		offset = (limit * page) || 0;
+		set(this, 'offset', offset);
+		return page + 1;
+	}.property('offset', 'limit'),
+
+	// 1-based
+	pages: function () {
+		var limit = get(this, 'limit');
+		var length = get(this, 'content.length');
+		var pages = Math.ceil(length / limit) || 1;
+		if (pages === Infinity) {
+			pages = 0;
 		}
-		pages *= -1;
-		return this.incrementPage(pages);
-	}
+		return pages;
+	}.property('content.length', 'limit')
 });
+
+ArrayPager.computed = {
+	page: function (prop, page, limit) {
+		return Em.computed(prop, function () {
+			return ArrayPager.create({
+				content: get(this, prop),
+				page: page,
+				limit: limit
+			});
+		});
+	}
+};
 
 export default ArrayPager;
