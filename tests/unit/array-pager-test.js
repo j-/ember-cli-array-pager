f41d2ddc082ebcaecdedefae845113c2ad9ad642
j- <jh@live.com.au>
2015-06-16 00:14:26 +1000

    Test content is initialized

--- tests/unit/array-pager-test.js
+++ tests/unit/array-pager-test.js
@@ -8,3 +8,14 @@ QUnit.test('constructor exists', function (assert) {
 	assert.ok(ArrayPager, 'ArrayPager is not null or undefined');
 	assert.equal(typeof ArrayPager, 'function', 'ArrayPager is function');
 });
+
+QUnit.test('content is initialized', function (assert) {
+	var arr = Em.A(['a', 'b', 'c']);
+	var slice = ArrayPager.create({
+		content: arr
+	});
+	assert.equal(slice.get('arrangedContent.length'), 3);
+	assert.equal(slice.get('content.length'), 3);
+	assert.equal(slice.get('length'), 3);
+	assert.deepEqual(slice.toArray(), ['a', 'b', 'c']);
+});
