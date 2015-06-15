b13dc2c159846e1d37bc783645d78603207f44c4
j- <jh@live.com.au>
2015-06-15 14:33:15 +1000

    Use get/set computed property pattern

--- addon/array-pager.js
+++ addon/array-pager.js
@@ -6,42 +6,47 @@ var set = Em.set;
 
 var ArrayPager = ArraySlice.extend({
 	// 1-based
-	page: computed('offset', 'limit', function (key, page) {
-		var limit = get(this, 'limit');
-		var offset;
-		// default value
-		if (arguments.length <= 1) {
-			offset = get(this, 'offset') || 0;
+	page: computed('offset', 'limit', {
+		get: function () {
+			var limit = get(this, 'limit');
+			var offset = get(this, 'offset') || 0;
 			return Math.ceil(offset / limit) || 1;
+		},
+		set: function (key, page) {
+			var limit = get(this, 'limit');
+			var offset = get(this, 'offset');
+			// no negative pages
+			page = Math.max(page - 1, 0);
+			offset = (limit * page) || 0;
+			set(this, 'offset', offset);
+			return page + 1;
 		}
-		// setter
-		// no negative pages
-		page = Math.max(page - 1, 0);
-		offset = (limit * page) || 0;
-		set(this, 'offset', offset);
-		return page + 1;
 	}),
 
 	// 1-based
-	pages: computed('content.length', 'limit', function () {
-		var limit = get(this, 'limit');
-		var length = get(this, 'content.length');
-		var pages = Math.ceil(length / limit) || 1;
-		if (pages === Infinity) {
-			pages = 0;
+	pages: computed('content.length', 'limit', {
+		get: function () {
+			var limit = get(this, 'limit');
+			var length = get(this, 'content.length');
+			var pages = Math.ceil(length / limit) || 1;
+			if (pages === Infinity) {
+				pages = 0;
+			}
+			return pages;
 		}
-		return pages;
 	})
 });
 
 ArrayPager.computed = {
 	page: function (prop, page, limit) {
-		return computed(function () {
-			return ArrayPager.create({
-				content: get(this, prop),
-				page: page,
-				limit: limit
-			});
+		return computed({
+			get: function () {
+				return ArrayPager.create({
+					content: get(this, prop),
+					page: page,
+					limit: limit
+				});
+			}
 		});
 	}
 };
