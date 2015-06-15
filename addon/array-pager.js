5a8be8f68a6e33deb81fb6f87ce1135038fd7292
j- <jh@live.com.au>
2015-06-15 14:34:32 +1000

    Alias math functions

--- addon/array-pager.js
+++ addon/array-pager.js
@@ -3,6 +3,8 @@ import ArraySlice from 'array-slice';
 var computed = Em.computed;
 var get = Em.get;
 var set = Em.set;
+var max = Math.max;
+var ceil = Math.ceil;
 
 var ArrayPager = ArraySlice.extend({
 	// 1-based
@@ -10,13 +12,13 @@ var ArrayPager = ArraySlice.extend({
 		get: function () {
 			var limit = get(this, 'limit');
 			var offset = get(this, 'offset') || 0;
-			return Math.ceil(offset / limit) || 1;
+			return ceil(offset / limit) || 1;
 		},
 		set: function (key, page) {
 			var limit = get(this, 'limit');
 			var offset = get(this, 'offset');
 			// no negative pages
-			page = Math.max(page - 1, 0);
+			page = max(page - 1, 0);
 			offset = (limit * page) || 0;
 			set(this, 'offset', offset);
 			return page + 1;
@@ -28,7 +30,7 @@ var ArrayPager = ArraySlice.extend({
 		get: function () {
 			var limit = get(this, 'limit');
 			var length = get(this, 'content.length');
-			var pages = Math.ceil(length / limit) || 1;
+			var pages = ceil(length / limit) || 1;
 			if (pages === Infinity) {
 				pages = 0;
 			}
